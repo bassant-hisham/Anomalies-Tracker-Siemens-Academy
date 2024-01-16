eb8562c7b0c525ca7ed3cb8f0b3238e103925bd3
naggar <72861717+naggarx@users.noreply.github.com>
2024-01-17 01:08:56 +0200

    edit

--- CustomWidgets/AddRuntime.py
+++ CustomWidgets/AddRuntime.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import *
-from CustomWidgets.RuntimeUI import Ui_MainWindow as Ui_Running_MainWindow
+from Runtime import Ui_MainWindow as Ui_Running_MainWindow
 from CustomRuntime import CustomRuntimeWindow
 from commonFunctions import *
 
