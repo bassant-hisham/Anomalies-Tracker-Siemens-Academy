2e74e52ed7c89e349d8340569c2a9eb409259909
naggar <72861717+naggarx@users.noreply.github.com>
2024-01-17 00:52:25 +0200

    main.py is added

---
+++ CustomWidgets/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+from PyQt5 import QtWidgets
+from mainwindow import MyMainWindow
+
+
+# In[ ]:
+
+
+#MAIN
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainwindow = MyMainWindow()
+    mainwindow.show()
+    sys.exit(app.exec_())
+
