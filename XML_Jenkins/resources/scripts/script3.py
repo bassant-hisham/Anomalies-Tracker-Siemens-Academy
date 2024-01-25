db197d55aa6db954b771453ff9a89a0ac07a8a89
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2024-01-25 20:41:21 +0200

    removed deleted files

--- XML_Jenkins/resources/scripts/script3.py
+++
@@ -1,9 +0,0 @@
-import time
-print("Executing script 3.1")
-time.sleep(30)
-print("Executing script 3.2")
-# time.sleep(30)
-# print("Executing script 3.3")
-# time.sleep(30)
-# print("Executing script 3.4")
-# time.sleep(30)
